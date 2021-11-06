9da0f8be9d6ba9478788eec678810b73ae698489
Sergey <formindness@gmail.com>
2021-11-06 09:21:54 +0600

    adding files

---
+++ self.sh
@@ -0,0 +1,11 @@
+#1/bin/sh
+#go
+cd homework;
+mkdir home1 home2 home3;
+cd home1;
+touch file1.txt; touch file2.txt; touch file3.txt;> file4.json; > file5.json;
+mkdir work1 work2 work3;
+ls;
+mv home1/file1.txt home2/file1.txt;
+mv home1/file2.txt home2/file2.txt
+
